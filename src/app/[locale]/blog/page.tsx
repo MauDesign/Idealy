e5b3a093447c00ddc6eb36252b5d75481b334660
MCDesign <contacto@mauriciocasado.com>
2026-04-15 13:24:53 -0600

    ajuste deply

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,6 +1,5 @@
 import { setRequestLocale } from 'next-intl/server';
 import { prisma } from '@/lib/prisma';
-import { Post } from '@prisma/client';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Calendar, Clock, ArrowRight, Rss, FolderOpen } from 'lucide-react';
